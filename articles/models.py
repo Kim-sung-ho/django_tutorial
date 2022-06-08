4532b20e6119c0c7cd248e9bed899d52c52dd778
DESKTOP-V6HUDEC\user <dbd02017@gmail.com>
2022-06-09 02:04:19 +0900

    no message

--- articles/models.py
+++ articles/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Article(models.Model): #상속
+    title = models.CharField(max_length=10)
+    #aksg
+    content = models.TextField()
+    create_at = models.DateTimeField(auto_now_add=True)
